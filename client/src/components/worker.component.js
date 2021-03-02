c45eabc0212d156570f37c7514a72e9097aebeb6
Blade Wolfmoon <2cpt1mp@gmail.com>
2021-03-03 00:59:15 +0300

    added linking for table rows and table effects

--- client/src/components/worker.component.js
+++ client/src/components/worker.component.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
 
 const WorkerRecord = (props) => {
+    const record = useRef();
+
+    const onMouseEnter = (event) => {
+        record.current.classList.add("table-active")
+    }
+
+    const onMouseLeave = (event) => {
+        record.current.classList.remove("table-active")
+    }
+
+    const onClick = (event) => {
+        window.location.href = `/api/workers/${props.id}`;
+    }
+
+
     return (
-       <tr>
+       <tr ref={record} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
            <td>{props.lastName}</td>
            <td>{props.firstName}</td>
            <td>{props.middleName}</td>
